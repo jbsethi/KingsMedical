4040fc7bd0ea048794438653b271d70f59161f0f
jahanzeb <jahanzebsethi@gmail.com>
2020-12-06 20:39:39 +0500

    Add Front end dist

---
+++ frontend/precache-manifest.3953cdf549d4006f86527282d931eedb.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c2195b3eedd62ed15e7",
+    "url": "/css/app.f6b391e2.css"
+  },
+  {
+    "revision": "75376bae85181f2b9c04",
+    "url": "/css/chunk-7109c3b4.a8445a3e.css"
+  },
+  {
+    "revision": "dc6a57f953e6f978c8b9",
+    "url": "/css/chunk-89df3564.457e07e1.css"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "b08f01c843779132fa416d0899665cc4",
+    "url": "/img/Human_dental_arches.b08f01c8.png"
+  },
+  {
+    "revision": "a794a15f6e45b941d8a521e7965221a8",
+    "url": "/img/brand/favicon.png"
+  },
+  {
+    "revision": "59d7c2b119ab46fc7b5f853f04324c89",
+    "url": "/img/brand/green.png"
+  },
+  {
+    "revision": "e0ea3f1916671872498288dc2bd40f06",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/img/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "1f533d2fdc40493ee834dd8e4288220c",
+    "url": "/img/theme/angular.jpg"
+  },
+  {
+    "revision": "d136a071cf09ba5ab8dc2ada1ab04015",
+    "url": "/img/theme/bootstrap.jpg"
+  },
+  {
+    "revision": "cee60cfd101f9b3d2aae0e819ec8b267",
+    "url": "/img/theme/profile-cover.jpg"
+  },
+  {
+    "revision": "50dd5fdc839dfc16739b15370650485c",
+    "url": "/img/theme/react.jpg"
+  },
+  {
+    "revision": "3b8c576082e5ca27bae21a839f1290f6",
+    "url": "/img/theme/sketch.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "c532f6f0176968caa6265864ee1abb35",
+    "url": "/img/theme/vue.jpg"
+  },
+  {
+    "revision": "e4f79fb93f8aece182f6666b89512e5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c2195b3eedd62ed15e7",
+    "url": "/js/app.41f3a854.js"
+  },
+  {
+    "revision": "4cb91175896c6fef6fdd",
+    "url": "/js/chunk-1471f473.3ef278e6.js"
+  },
+  {
+    "revision": "7405500723f732e02651",
+    "url": "/js/chunk-4a0f5685.9917be27.js"
+  },
+  {
+    "revision": "75376bae85181f2b9c04",
+    "url": "/js/chunk-7109c3b4.018cf6c5.js"
+  },
+  {
+    "revision": "dc6a57f953e6f978c8b9",
+    "url": "/js/chunk-89df3564.d40c455d.js"
+  },
+  {
+    "revision": "69b33a46158f556b6b92",
+    "url": "/js/chunk-vendors.0293e9dc.js"
+  },
+  {
+    "revision": "2c6190eb814d2dd4d454ae170c43c7c2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
